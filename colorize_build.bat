bea5ea59e7d665b5af83c47784cb4370b873a02b
nijel8 <nijel8@gmail.com>
2025-02-23 10:54:43 -0500

    colorize_build.bat - build static exe, fix missing DLLs

--- colorize_build.bat
+++ colorize_build.bat
@@ -5,7 +5,7 @@ echo "Building coloryze.exe..."
 RMDIR /S /Q build >NUL
 MKDIR build >NUL
 
-g++ -O3 -static-libgcc -static-libstdc++ -o build\tmp.exe colorize.cpp
+g++ -O3 -static-libgcc -static-libstdc++ --static -o build\tmp.exe colorize.cpp
 if NOT ["%errorlevel%"]==["0"] RMDIR /S /Q build >NUL && pause
 
 strip colorize\tmp.exe
